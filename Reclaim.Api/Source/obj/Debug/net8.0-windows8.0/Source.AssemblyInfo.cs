b1997189cbcf8f91a4a422f2e69703def529ba5f
briandev85 <christensenbrian83@gmail.com>
2025-02-07 05:57:57 -0800

    update chatbot

--- Reclaim.Api/Source/obj/Debug/net8.0-windows8.0/Source.AssemblyInfo.cs
+++ Reclaim.Api/Source/obj/Debug/net8.0-windows8.0/Source.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Source")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be1a69fc2a4d39632d9eeade8dd600e77e97a7ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d340882c2c4c3d6355984ebfc7bfda6f8c18593")]
 [assembly: System.Reflection.AssemblyProductAttribute("Source")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Source")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
